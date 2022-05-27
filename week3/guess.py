ed82811fb43de942657314879f8bcec1bf9f0ba6
Ursula.Netherland <ursula.netherland@bryantuniversity.edu>
2022-05-27 12:33:14 -0700

    week3

--- week3/guess.py
+++ week3/guess.py
@@ -1,6 +1,5 @@
 
-step1 = input("Can you guess the secret occupation?")
-print(step1)
+
 
 word ="secretary" 
 
@@ -12,13 +11,32 @@ mylist = []
 def snowBoard ():
    print(wordBoard)    
 
-def checkGuess(guessedLetter):
+def checkGuess(guessedLetter, lives):
     foundRec = []
     position = 0
     for letter in word:
         if letter == guessedLetter: 
             foundRec.append(position)
+            print(position)
         position += 1
     print(foundRec)
+    if (len(foundRec)<1):
+        lives = lives-1
+        
+    for index in foundRec:
+        wordBoard[index] = guessedLetter
 
-checkGuess ("e")
+    print(wordBoard)
+    print(lives)
+           
+win = False
+lives = 5 
+
+print("Can you guess the secret occupation?")
+while (win == False and lives>1):
+
+    step1 = input("Guess a letter, ")
+    print(step1)
+
+    checkGuess (step1, lives)
+
