a3b0af727f15f2da73fdbb4cf94049a82192bb73
Ursula.Netherland <ursula.netherland@bryantuniversity.edu>
2022-05-27 15:03:52 -0700

    week3

--- week3/guess.py
+++ week3/guess.py
@@ -7,11 +7,14 @@ wordBoard = ["_","_","_","_","_","_","_","_","_"]
 print(wordBoard)
 
 mylist = []
+win = False
+lives = 5 
 
 def snowBoard ():
    print(wordBoard)    
 
 def checkGuess(guessedLetter, lives):
+    print(lives)
     foundRec = []
     position = 0
     for letter in word:
@@ -21,22 +24,40 @@ def checkGuess(guessedLetter, lives):
         position += 1
     print(foundRec)
     if (len(foundRec)<1):
-        lives = lives-1
-        
-    for index in foundRec:
-        wordBoard[index] = guessedLetter
+        lives -= 1
+        return lives
+    else:
 
-    print(wordBoard)
-    print(lives)
-           
-win = False
-lives = 5 
+        for index in foundRec:
+            wordBoard[index] = guessedLetter
+
+        counter = 0
+        for blank in wordBoard:
+            if blank !="_":
+                counter +=1
+        
+        if counter == len(word):
+           return True
+        else:
+            return False
 
 print("Can you guess the secret occupation?")
-while (win == False and lives>1):
+while (win == False and lives>0):
 
-    step1 = input("Guess a letter, ")
-    print(step1)
+    guess = input("Guess a letter, ")
+    print(guess)
 
-    checkGuess (step1, lives)
+    result = checkGuess (guess,lives)
+    print(type(result))
+    if type(result) is int:
+        lives = result
+    else:
+        win = result 
+       
+    snowBoard ()  
 
+print(lives, win)
+if win ==True:
+    print('Winner.')
+else:
+    print('You loose.')
